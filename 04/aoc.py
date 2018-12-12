cd41eca4a7a529924505acfe6cd8ae6ec6f7585c
Crestar <erni-services.adrian.crestar@hp.com>
2018-12-12 22:53:44 +0100

    AoC 04 Second Part

--- 04/aoc.py
+++ 04/aoc.py
@@ -45,69 +45,94 @@ class Guard():
         
     def mostSleepedMinute(self):
         try:
-            print(self._minutes)
+            # print(self._minutes)
             return self._minutes.index(max(self._minutes))
         except ValueError:
             return None
-
-class GuardCollection():
-   _guards = None
-   _guardMostAsleep = -1
-   
-   def __init__(self, guards):
-        self._guards = guards
-        
-        
-def sortGuardsLogs(input):
-    dict = {}
-    with open(input, 'r') as f:
-        for line in f:
-            date = line[1:line.find("]")];
-            # On Windows, the minimum accepted year is 1970 (in case you see an error here)
-            tstamp = datetime.datetime.strptime(date, "%Y-%m-%d %H:%M").timestamp()
-            dict[tstamp] = line[line.find("]") + 1:]
-            # print("Date is {0}".format(date))
-            # print(tstamp)
-            # print(dict[tstamp])
-        
-    return collections.OrderedDict(sorted(dict.items()))
-
-# Having a timestamp, for example: 111111111112 (15:00:01)
-# and doing '111111111112 - 1' -> 15:00:00 !!!
-def buildGuards(sortedLogs):
-    guards = {}
-    lastGuardOnDuty = -1
-    for tstamp, log in sortedLogs.items():
-        if "begins shift" in log:
-            id = int(log[log.find("#") + 1:log.find(" ", log.find("#"))])
-            # print("id is {0}".format(id))
-            if id not in guards:
-                guards[id] = Guard(id)
-                
-            lastGuardOnDuty = id
-        elif "falls asleep" in log:
-            min = datetime.datetime.fromtimestamp(tstamp).minute
-            # print(min)
-            guards[lastGuardOnDuty].fallsAsleep(min)
-        elif "wakes up" in log:
-            lastMinuteAsleep = datetime.datetime.fromtimestamp(tstamp).minute
-            # print(lastMinuteAsleep)
-            guards[lastGuardOnDuty].wakesUp(lastMinuteAsleep)
             
-    return guards
+    def mostSleepedMinuteTimes(self):
+        return (self.mostSleepedMinute(), max(self._minutes))
 
-def parseInputFile(input):
-    sortedLogs = sortGuardsLogs(input)
-    return buildGuards(sortedLogs)
+class GuardsShifts():
+    _guards = None
+        
+    def __init__(self, inputFile):
+        self._guards = self._buildGuards(inputFile)
+    
+    def _parseGuardsLogs(self, input):
+        dict = {}
+        with open(input, 'r') as f:
+            for line in f:
+                date = line[1:line.find("]")];
+                # On Windows, the minimum accepted year is 1970 (in case you see an error here)
+                tstamp = datetime.datetime.strptime(date, "%Y-%m-%d %H:%M").timestamp()
+                dict[tstamp] = line[line.find("]") + 1:]
+                # print("Date is {0}".format(date))
+                # print(tstamp)
+                # print(dict[tstamp])
+            
+        return collections.OrderedDict(sorted(dict.items()))
+
+    # Having a timestamp, for example: 111111111112 (15:00:01)
+    # and doing '111111111112 - 1' -> 15:00:00 !!!
+    def _buildGuards(self, inputFile):
+        sortedLogs = self._parseGuardsLogs(inputFile)
+        guards = {}
+        lastGuardOnDuty = -1
+        
+        for tstamp, log in sortedLogs.items():
+            if "begins shift" in log:
+                id = int(log[log.find("#") + 1:log.find(" ", log.find("#"))])
+                # print("id is {0}".format(id))
+                if id not in guards:
+                    guards[id] = Guard(id)
+                    
+                lastGuardOnDuty = id
+            elif "falls asleep" in log:
+                min = datetime.datetime.fromtimestamp(tstamp).minute
+                # print(min)
+                guards[lastGuardOnDuty].fallsAsleep(min)
+            elif "wakes up" in log:
+                lastMinuteAsleep = datetime.datetime.fromtimestamp(tstamp).minute
+                # print(lastMinuteAsleep)
+                guards[lastGuardOnDuty].wakesUp(lastMinuteAsleep)
+                
+        return guards
+    
+    def findGuardSleepingMost(self):
+        sleepedMinutes = 0
+        guardSleepingMost = -1
+        
+        for k, g in self._guards.items():
+            if g.totalMinutesAsleep > sleepedMinutes:
+                sleepedMinutes = g.totalMinutesAsleep
+                guardSleepingMost = g.id
+                
+        return self._guards[guardSleepingMost]
+        
+    def findGuardMoreTimesAsleepTheSameMinute(self):
+        asleepGuard = -1
+        mostSleepedMinute = -1
+        timesSleeping = -1
+        
+        for k, g in self._guards.items():
+            minute, times = g.mostSleepedMinuteTimes()
+            if times > timesSleeping:
+                timesSleeping = times
+                mostSleepedMinute = minute
+                asleepGuard = g.id
+                
+        return self._guards[asleepGuard]
     
 if __name__ == __name__:
-    guards = parseInputFile(sys.argv[1])
-    guardMostAsleep = -1
-    previousMostSleepedMinutes = 0
+    shifts = GuardsShifts(sys.argv[1])
     
-    for k, g in guards.items():
-        if g.totalMinutesAsleep > previousMostSleepedMinutes:
-            previousMostSleepedMinutes = g.totalMinutesAsleep
-            guardMostAsleep = g.id
-            
-    print("guard id: {0} - mins: {1} - most sleepws minute: {2}".format(guardMostAsleep, guards[guardMostAsleep].totalMinutesAsleep, guards[guardMostAsleep].mostSleepedMinute()))
+    lazyGuard = shifts.findGuardSleepingMost()
+    print("Strategy 1 || guard id: {0} - mins: {1} - most sleeped minute: {2}".format(lazyGuard.id, lazyGuard.totalMinutesAsleep, lazyGuard.mostSleepedMinute()))
+    print("\tguard id * mostSleepedMinute: {0}".format(lazyGuard.id * lazyGuard.mostSleepedMinute()))
+    print()
+    
+    lazyGuard = shifts.findGuardMoreTimesAsleepTheSameMinute()
+    mostSleepedMinute, timesSleeping = lazyGuard.mostSleepedMinuteTimes()
+    print("Strategy 2 || guard id: {0} - most sleeped minute: {1} - total times: {2}".format(lazyGuard.id, mostSleepedMinute, timesSleeping))
+    print("\tguard id * most sleeped minute: {0}".format(lazyGuard.id * mostSleepedMinute))
